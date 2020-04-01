8d4051904ebe2543c1b5dc516908d48ceeda3283
karaage23bento33 <mahalo.morganite33@gmail.com>
2020-04-01 20:08:28 +0900

    vegas.option.jsのパスを../から./へ変更

--- js/vegas.option.js
+++ js/vegas.option.js
@@ -1,11 +1,11 @@
 $(function() {
   $('#vegas').vegas({
     slides: [
-      { src: '../images/vegas_sample01.jpg' },
-      { src: '../images/vegas_sample02.jpg' },
-      { src: '../images/vegas_sample03.jpg' },
+      { src: './images/vegas_sample01.jpg' },
+      { src: './images/vegas_sample02.jpg' },
+      { src: './images/vegas_sample03.jpg' },
     ],
-    overlay: '../overlays/02.png', //フォルダ『overlays』の中からオーバーレイのパターン画像を選択
+    overlay: './overlays/02.png', //フォルダ『overlays』の中からオーバーレイのパターン画像を選択
     transition: 'fade', //スライドを遷移させる際のアニメーション
     transitionDuration: 4000, //スライドの遷移アニメーションの時間
     delay: 10000, //スライド切り替え時の遅延時間
